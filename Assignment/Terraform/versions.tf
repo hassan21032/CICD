68389d6b83867022d0cd81564cba1be92693b2f2
hassan21032 <hassan@noorhten.com>
2026-03-04 20:25:28 +0000

    Add S3 backend for Terraform state

--- Assignment/Terraform/versions.tf
+++ Assignment/Terraform/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket = "hassan21032-terraform-state"
+    key    = "cicd/ecr/terraform.tfstate"
+    region = "eu-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
