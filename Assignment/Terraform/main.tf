16dfb77ec35cf2584d53521fdfcad943a1bfd2f0
hassan21032 <hassan@noorhten.com>
2026-03-04 19:51:54 +0000

    Fix checkov security findings - KMS encryption and immutable tags

--- Assignment/Terraform/main.tf
+++ Assignment/Terraform/main.tf
@@ -1,9 +1,13 @@
 resource "aws_ecr_repository" "hello_app" {
   name                 = var.ecr_repo_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
   }
 
   tags = {
